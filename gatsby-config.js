893b7071c463b1613b4b888b45a78cb7e8ffade8
Robbie Cook <robbieck7@gmail.com>
2020-05-31 20:31:51 +1200

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	siteMetadata: {
+		title: `Poetry.digital`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		// Add typescript stack into webpack
+		`gatsby-plugin-typescript`,
+		`gatsby-plugin-styled-components`
+	],
+};
