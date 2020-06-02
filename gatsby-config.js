34f9c0f9e1af65e06e8c0d296a61e6e19a30ec29
Robbie Cook <robbieck7@gmail.com>
2020-06-02 22:12:38 +1200

    Started creating basic layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,35 @@
 module.exports = {
-	siteMetadata: {
-		title: `Poetry.digital`,
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		// Add typescript stack into webpack
-		`gatsby-plugin-typescript`,
-		`gatsby-plugin-styled-components`
-	],
-};
+  siteMetadata: {
+    title: `Poetry.digital`,
+    description: `A collection of poetry on the web.`,
+    author: `Robbie Cook`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `poetry.digital`,
+        short_name: `A collection of poetry on the web.`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/components/header.js
@@ -0,0 +1,44 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core"
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0.8rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
